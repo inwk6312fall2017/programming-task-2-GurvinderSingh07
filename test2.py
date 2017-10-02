5fdcadbbdc9c81c499944b6ee904aba21fa8be7d
inwkstudent <kalsiashish@yahoo.com>
2017-10-02 12:57:08 -0300

    edited2

--- test2.py
+++ test2.py
@@ -1,5 +1,9 @@
 #import csv to get the functions for csv file 
-ASSAULT=0
+#ASSAULT=0
+#ROBBERY=0
+#THEFT FROM VEHICLE=0
+#BREAK AND ENTER=0
+import string
 import csv
 #file=open('Crime.csv')
 #to calculate total no of assault crime happened in last 7 days
@@ -8,7 +12,11 @@ import csv
 with open('Crime.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row['	crime1'])
+            #print(row['	crime1'])
+		dict=d()
+		d[row[8]==row[7]]
+		print(d)
+		
 	if('crime1'== 1):
 		'crime1'ASSAULT +=1
 	else:
